91a5ee927ada25b14527650ae3dae3b2c0a98baa
usuario <juanfedo@yahoo.com>
2019-05-07 06:51:45 -0500

    proyecto de trabajo 2 MCIC

---
+++ facial_landmarks/archivos.py
@@ -0,0 +1,10 @@
+import os
+
+directorios = []
+for name in os.listdir("."):
+    if os.path.isdir(name):
+        directorios.append(os.path.abspath(name)+'/0.Hombres')
+        directorios.append(os.path.abspath(name)+'/1.Mujeres')
+        print (os.path.abspath(name)+"/0.Hombres")
+        print (os.path.abspath(name)+"/1.Mujeres")
+#print [ for name in os.listdir(".") if os.path.isdir(name)]
