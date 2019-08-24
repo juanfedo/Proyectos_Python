738058c456bcb7fbda2071bcd48a566e1e98f8a3
juanfedo <juanfedo@yahoo.com>
2019-08-24 10:38:27 -0500

    se crea index.py con mayor exactitud en el reconocimiento

--- Client_Server_face_detection_sell/Client/index.py
+++ Client_Server_face_detection_sell/Client/index.py
@@ -3,30 +3,29 @@ import sys
 from time import sleep
 import requests
 import base64
+import face_recognition
 
-cascPath = sys.argv[1]
-faceCascade = cv2.CascadeClassifier(cascPath)
 video_capture = cv2.VideoCapture(0)
 while True:    
 	sleep(0.1)
 	ret, frame = video_capture.read()
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	faces = faceCascade.detectMultiScale(
-		gray,
-		scaleFactor=1.1,
-		minNeighbors=5,
-		minSize=(30, 30),
-		flags=cv2.CASCADE_SCALE_IMAGE
-	)	
-	if len(faces) > 0:
+	small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)    
+	rgb_small_frame = small_frame[:, :, ::-1]
+	face_locations = face_recognition.face_locations(rgb_small_frame)
+	print ("face_locations")
+	print (face_locations)
+	if len(face_locations) > 0:
 		cv2.imwrite('capture.jpg', frame)
 		with open("capture.jpg", "rb") as imageFile:
-			img = base64.b64encode(imageFile.read())
-		#img = base64.b64encode(frame)
+			img = base64.b64encode(imageFile.read())		
 		response = requests.post("http://127.0.0.1:5000/add_face", data={"name":"127.0.0.cliente", "img":str(img)})
 		print ('call webservice and recive from server: {}'.format(response.content))
-	for (x, y, w, h) in faces:
-		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+	for (top, right, bottom, left) in face_locations:
+		left*=2
+		right*=2
+		top*=2
+		bottom*=2
+		cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)		
 	cv2.imshow('Video', frame)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
---
+++ Client_Server_face_detection_sell/Client/old_version/test_cliente_face_detection.py
@@ -0,0 +1,34 @@
+# import cv2
+# import sys
+# from time import sleep
+# import requests
+# import base64
+
+# cascPath = sys.argv[1]
+# faceCascade = cv2.CascadeClassifier(cascPath)
+# video_capture = cv2.VideoCapture(0)
+# while True:    
+# 	sleep(0.1)
+# 	ret, frame = video_capture.read()
+# 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# 	faces = faceCascade.detectMultiScale(
+# 		gray,
+# 		scaleFactor=1.1,
+# 		minNeighbors=5,
+# 		minSize=(30, 30),
+# 		flags=cv2.CASCADE_SCALE_IMAGE
+# 	)	
+# 	if len(faces) > 0:
+# 		cv2.imwrite('capture.jpg', frame)
+# 		with open("capture.jpg", "rb") as imageFile:
+# 			img = base64.b64encode(imageFile.read())
+# 		#img = base64.b64encode(frame)
+# 		response = requests.post("http://127.0.0.1:5000/add_face", data={"name":"127.0.0.cliente", "img":str(img)})
+# 		print ('call webservice and recive from server: {}'.format(response.content))
+# 	for (x, y, w, h) in faces:
+# 		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+# 	cv2.imshow('Video', frame)
+# 	if cv2.waitKey(1) & 0xFF == ord('q'):
+# 		break
+# video_capture.release()
+# cv2.destroyAllWindows()
